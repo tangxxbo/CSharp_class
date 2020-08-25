408cc91758d35e33610160ea5ac2cdba3dee8781
晓旭 唐 <1165907265@qq.com>
2020-08-25 10:42:12 +0800

    学习

--- CSharp_class/Form1.cs
+++ CSharp_class/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,6 +20,61 @@ namespace CSharp_class
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            /*ArrayList arrayList = new ArrayList();
+            arrayList.Add(123);
+            arrayList.Add("wc");
+            arrayList.Insert(1, "fk");
+            arrayList.RemoveAt(0);
+            arrayList.Remove("wc");*/
+            /*Person person = new Person()
+            {
+                Name = "李四",
+                Age = 16,
+                Height = 180
+            };
+
+            person.Age = 18;
+            person.Height = 175;
+            person.Name = "王五";
+
+            string sports = Person.Sport = "篮球";
+            Person.Play(sports);*/
+
+            List<Person> people = new List<Person>();
+            Person person = new Person();
+            person.Age = 18;
+            person.Height = 175;
+            person.Name = "王五";
+            Person.Sport = "basketball";
+            people.Add(person);
+
+            Dictionary<int, string> dicts1 = new Dictionary<int, string>();
+            dicts1.Add(1,"DFP");
+            dicts1.Add(2, "TXX");
+            dicts1.Add(3, "XQ");
+            dicts1[1] = "DLB";
+            dicts1[4] = "HXW";
+
+            Dictionary<string, string> dicts2 = new Dictionary<string, string>() {
+                { "湘大", "DFP"},
+                { "中南", "TXX"},
+                { "湖大", "XQ"},
+                {"师范", "HXW" },
+                { "科大", "DK"}
+            };
+            Dictionary<string, string> dicts3 = new Dictionary<string, string>();
+            dicts3.Add("湘大", "DFP");
+            dicts3.Add("中南", "TXX");
+            dicts3.Add("湖大", "XQ");
+            dicts3.Add("师范", "HXW");
+            dicts3.Add("科大", "DK");
+
+            foreach (KeyValuePair<string,string> item in dicts3)
+            {
+                string key = item.Key;
+                string value = item.Value;
+                MessageBox.Show(key + ":" + value);
+            }
 
         }
     }
---
+++ CSharp_class/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_class
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Height { get; set; }
+
+        public static string Sport { get; set; }
+
+        public void Introduce(string name)
+        {
+            MessageBox.Show("我叫" + name);
+        }
+        public string Tops(int height)
+        {
+            return  "身高"+ height+"CM";
+        }
+
+        public string Eat(string name)
+        {
+            return name + "正在吃饭";
+        }
+
+        public string Run(string name)
+        {
+            return name + "正在跑步";
+        }
+
+        public static void Play(string name)
+        {
+            MessageBox.Show(name + "正在玩");
+        }
+
+    }
+}
